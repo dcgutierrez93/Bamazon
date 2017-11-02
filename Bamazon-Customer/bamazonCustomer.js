c5760087ebddc1e65dc7b6f592a9f1bb1dd7b88d
David Gutierrez <dcgutierrez93@yahoo.com>
2017-11-02 09:54:01 -0500

    Installed Package.json and Npnpm

--- Bamazon-Customer/bamazonCustomer.js
+++ Bamazon-Customer/bamazonCustomer.js
@@ -1,5 +1,25 @@
 // =============================================================================
 
-
+// Initial npm packages
 
 // =============================================================================
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require('console.table');
+
+// Initialize connection to MYSQL database.
+const connection = mysql.createConnection({
+    host    : "localhost",
+    port    : 3306,
+    user    : "root",
+    password: "root",
+    database: "bamazon"
+});
+
+// Create the connection to the server & load products.
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+    }
+    loadProducts();
+})
